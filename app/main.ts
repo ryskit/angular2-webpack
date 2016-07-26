7fc340c0124eab986de852302dd573fe2d4d85f2
Kida <kida@zyyx.jp>
2016-07-26 15:57:33 +0900

    service に分離

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,42 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
+import { Component, Injectable } from '@angular/core';
+import { HTTP_PROVIDERS, Http, Request, Response } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/operator/map';
 
-bootstrap(AppComponent);
+interface Sample {
+  name: string;
+  age: number;
+}
+
+@Injectable()
+class SampleService {
+
+  constructor(private http: Http) {}
+
+  fetch(): Observable<Sample> {
+    return this.http.get("./data.json").map(res=>res.json() as Sample);
+  }
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h3>Http Example</h3>
+    <pre>{{ data | json }}</pre>
+  `,
+  providers: [SampleService]
+})
+
+export class MyApp {
+  public data: Sample;
+
+  constructor(private service: SampleService) {}
+
+  ngOnInit() {
+    this.service.fetch().subscribe(data => this.data = data);
+  }
+}
+
+bootstrap(MyApp, [HTTP_PROVIDERS]);
